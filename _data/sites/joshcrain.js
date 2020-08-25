fca697390f17ea38889678ae5b5e15e2149d04ef
Joshua Crain <joshuacrain@MacBook-Pro.local>
2020-08-25 05:45:15 -0500

    add joshcrain

---
+++ _data/sites/joshcrain.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "Josh Crain", // optional, falls back to object key
+	description: "Josh Crain sites to get tested ",
+	skip: false,
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://joshcrain.io/",
+		"https://joshcrain.io/ancestry/"
+	]
+};
